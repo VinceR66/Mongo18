f5509567ee47a2d02b1b96a94c0120b8a77addfe
VinceR66 <vrook@me.com>
2023-08-21 13:14:45 -0500

    final

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -23,7 +23,7 @@ const thoughtController = {
         Thought.find({})
             .populate({ path: 'reactions', select: '-__v' })
             .select('-__v')
-            // .sort({_id: -1})
+
             .then(dbThoughtData => res.json(dbThoughtData))
             .catch(err => {
                 console.log(err);
